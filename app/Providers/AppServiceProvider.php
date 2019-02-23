109c82511ee5f897753eb291ff62a962161ce026
sakib <md.sakibrahman@gmail.com>
2019-02-23 15:30:22 +0600

    sakib

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,7 +19,7 @@ class AppServiceProvider extends ServiceProvider
         {
             $project = Project::where('home','1')->take(5)->orderBy('created_at','desc')->get();
             $allProject = Project::get();
-            $projectName = Project::select('pName','projectId','slug')->orderBy('serial','asc')->get();
+            $projectName = Project::select('pName','projectId','slug')->orderBy('serial','asc')->where('active', 1)->get();
             $rogingaprojectName = RohingyaProject::select('title','rohingyaprojectId')->orderBy('serial','asc')->get();
             $view->with('projectsProvider',$project)->with('allProjects',$allProject)->with('projectName',$projectName)->with('rogingaprojectname', $rogingaprojectName);
         });
